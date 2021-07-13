4baec903361363479de1305aab53dc03b1cf6c41
Aleksey Titov <talexden@gmail.com>
2021-07-13 14:51:35 +0300

    проверка 2 поправил имя

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import {createGaleria} from './galeria/galeria.js';
-import {ConnectModuleForm} from './upload-form/upload-form.js';
+import {connectModuleForm} from './upload-form/upload-form.js';
 
 createGaleria();
-ConnectModuleForm();
+connectModuleForm();
